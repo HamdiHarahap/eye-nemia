edaa4534d6184fb68b75455ddff09a934aafdd33
HamdiHarahap <hamdiharahap2005@gmail.com>
2026-05-03 13:15:38 +0700

    perbaiki validasi respons pada fungsi registrasi dan login

--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -9,9 +9,26 @@ export const register = async (name, username, password) => {
 
   const data = await response.json();
 
-  if (!response.ok) {
+  if (data.status !== 'success') {
     throw new Error(data.message || 'Registrasi Gagal');
   }
+
+  return data;
+};
+
+export const login = async (username, password) => {
+  const response = await fetch(`${BASE_URL}/authentications`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const data = await response.json();
+
+  if (data.status !== 'success') {
+    throw new Error(data.message || 'Login Gagal');
+  }
+  console.log(data);
 
   return data;
 };
