23b6e771a0c5c8927e81d3a87219f2207785644b
HamdiHarahap <hamdiharahap2005@gmail.com>
2026-05-03 21:47:17 +0700

    menambahkan fungsi logout dan getUserLogged untuk mengelola sesi pengguna

--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -1,3 +1,6 @@
+import { getRefreshToken } from '../../utils/token.js';
+import { fetchWithToken } from '../../utils/fetchWithToken.js';
+
 const BASE_URL = 'http://localhost:3000';
 
 export const register = async (name, username, password) => {
@@ -31,4 +34,38 @@ export const login = async (username, password) => {
   console.log(data);
 
   return data;
+};
+
+export const logout = async () => {
+  const refreshToken = getRefreshToken();
+
+  const response = await fetch(`${BASE_URL}/authentications`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ refreshToken }),
+  });
+
+  const data = await response.json();
+
+  if (data.status !== 'success') {
+    throw new Error(data.message || 'Logout gagal');
+  }
+
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
+  localStorage.removeItem('userId');
+};
+
+export const getUserLogged = async () => {
+  const response = await fetchWithToken(`${BASE_URL}/users/me`);
+
+  const data = await response.json();
+
+  if (data.status !== 'success') {
+    throw new Error(data.message);
+  }
+
+  return data.data;
 };
