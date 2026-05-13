9dcf095f72248e50b5d16eb4cdaeb6a2d65efefa
HamdiHarahap <hamdiharahap2005@gmail.com>
2026-05-13 22:38:46 +0700

    menambahkan hook useDocumentTitle untuk mengatur judul halaman Step 5

--- src/pages/Screnning/Step5.jsx
+++ src/pages/Screnning/Step5.jsx
@@ -3,8 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import MainLayout from '../../layout/index';
 import StepProgress from '../../components/StepProgress';
 import { useScreening } from '../../context/ScreeningContext';
+import useDocumentTitle from '../../hooks/useDocumentTitle';
 
 const Step5 = () => {
+  useDocumentTitle('Screening | EyeNemia');
+
   const navigate = useNavigate();
   const { formData, updateFormData } = useScreening();
   const [errors, setErrors] = useState({});
